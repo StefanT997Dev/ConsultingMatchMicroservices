3f94d6f9d0950bcbe44bd4e40d62d868c2c59426
Stefan <stefantosic.dev@gmail.com>
2021-06-18 12:10:47 +0200

    AddedSkillsController

---
+++ client-app/src/stores/commonStore.ts
@@ -0,0 +1,28 @@
+import { makeAutoObservable } from "mobx";
+
+export default class CommonStore{
+    displayConsultantContact:boolean=false;
+    token:string|null=window.localStorage.getItem('jwt');
+    appLoaded=false;
+
+    constructor(){
+        makeAutoObservable(this);
+
+        // reaction(
+        //     ()=>
+        // )
+    }
+
+    setDisplayConsultantContact=()=>{
+        this.displayConsultantContact=!this.displayConsultantContact;
+    }
+
+    setToken=(token:string|null)=>{
+        if(token) window.localStorage.setItem('jwt',token);
+        this.token=token;
+    }
+
+    setAppLoaded=()=>{
+        this.appLoaded=true;
+    }
+}
