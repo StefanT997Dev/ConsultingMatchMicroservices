cd56e5c38ea2980c054474575481ddc9164d6ab8
Stefan <stefantosic.dev@gmail.com>
2021-11-23 05:48:48 +0100

    Remove frontend

--- client-app/src/models/post.ts
+++
@@ -1,8 +0,0 @@
-export interface Post{
-    id:string,
-    title:string,
-    description:string | number | undefined,
-    picture?:string,
-    video?:string,
-    numberOfCredits?:number
-}
--- client-app/src/util/data.ts
+++
@@ -1,6 +0,0 @@
-export const isEmptyObject = (obj:{}) => {
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) return false;
-  }
-  return true;
-};
