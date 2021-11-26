d4fae7a9f0b679503e1fedc665ab952a89f1b9b1
Stefan <stefantosic.dev@gmail.com>
2021-11-26 12:44:46 +0100

    Change CORS policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -46,7 +46,7 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy",policy=>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000", "https://it-mentor-ui.herokuapp.com");
                 });
             });
 
