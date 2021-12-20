78d08e15751319245d55003889082d73b8a86120
Stefan <stefantosic.dev@gmail.com>
2021-12-20 07:23:12 +0100

    test get mentors paginated

--- Infrastructure.UnitTests/MentorsRepositoryTests.cs
+++ Infrastructure.UnitTests/MentorsRepositoryTests.cs
@@ -1,8 +1,10 @@
 using Application.Core;
+using AutoFixture;
 using AutoMapper;
 using Domain;
 using Infrastructure.RepositoriesImpl;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Persistence;
 using System;
 using System.Linq;
@@ -13,57 +15,57 @@ namespace Infrastructure.UnitTests
 {
 	public class MentorsRepositoryTests
 	{
-		[Fact]
-		public async Task GetMentorsPaginatedAsync_s_s()
+		[Theory]
+		[InlineData(0, false)]
+		[InlineData(5, true)]
+		public async Task GetMentorsPaginatedAsync_MentorsExistOrNot_ReturnsAdequeateResult(
+			int numberOfMentors,
+			bool returnsMentors
+			)
 		{
-            var options = new DbContextOptionsBuilder<DataContext>()
-            .UseInMemoryDatabase(databaseName: "DataContextDB")
-            .Options;
 			var mappingConfig = new MapperConfiguration(mc =>
 			{
 				mc.AddProfile(new MappingProfiles());
 			});
+			var fixture = new Fixture();
+			fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+			fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+			var users = fixture
+				.Build<AppUser>()
+				.With(u => u.Role, new Role { Id = 1, Name = "Mentor", Users = null })
+				.CreateMany(numberOfMentors);
 
 			// Insert seed data into the database using one instance of the context
-			using (var context = new DataContext(options))
+			using (var context = new DataContext(CreateNewContextOptions()))
 			{
-				context.Users.AddRange(new AppUser
-				{
-					DisplayName = "Bob",
-					UserName = "bob",
-					Email = "bob@test.com",
-					Bio = "I am Bob and I'm a software engineer",
-					Role = new Role { Name = "Mentor" }
-				},
-				new AppUser
-				{
-					DisplayName = "Tom",
-					UserName = "tom",
-					Email = "tom@test.com",
-					Bio = "I am Tom and I'm a software engineer",
-					Role = new Role { Name = "Mentor" }
-				},
-				new AppUser
-				{
-					DisplayName = "John",
-					UserName = "john",
-					Email = "john@test.com",
-					Bio = "I am John and I'm a software engineer",
-					Role = new Role { Name = "Mentor" }
-				});
+				context.Users.AddRange(users);
 				context.SaveChanges();
-			}
 
-			// Use a clean instance of the context to run the test
-			using (var context = new DataContext(options))
-			{
 				MentorsRepository mentorsRepository = new MentorsRepository(context, mappingConfig.CreateMapper());
 				var mentors = await mentorsRepository.GetMentorsPaginatedAsync(1, 5, null);
 
 				var mentorsList = mentors.Item1.ToList();
 
-				Assert.Equal(3, mentorsList.Count);
+				Assert.Equal(returnsMentors, mentorsList.Any());
 			}
+		}
+		private static DbContextOptions<DataContext> CreateNewContextOptions()
+		{
+			// Create a fresh service provider, and therefore a fresh 
+			// InMemory database instance.
+			var fixture = new Fixture();
+			var dbName = fixture.Build<string>().Create();
+			var serviceProvider = new ServiceCollection()
+				.AddEntityFrameworkInMemoryDatabase()
+				.BuildServiceProvider();
+
+			// Create a new options instance telling the context to use an
+			// InMemory database and the new service provider.
+			var builder = new DbContextOptionsBuilder<DataContext>();
+			builder.UseInMemoryDatabase(dbName)
+				   .UseInternalServiceProvider(serviceProvider);
+
+			return builder.Options;
 		}
 	}
 }
